9238027eda6752e2afc16a021d99d3094c10ef56
snoopy910 <snoopy5473cool@gmail.com>
2024-05-23 20:27:21 +0530

    revert cors

--- sequencer/api.go
+++ sequencer/api.go
@@ -66,8 +66,8 @@ type GetAddressResponse struct {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "*")
-	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
 func startHTTPServer(port string) {

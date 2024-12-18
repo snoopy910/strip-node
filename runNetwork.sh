015603e1adca644ac5f987c57d0cb9545d7db8f0
snoopy910 <snoopy5473cool@gmail.com>
2024-12-18 19:51:56 +0200

    fix/return the runNetwork.sh

--- runNetwork.sh
+++ runNetwork.sh
@@ -1,3 +1,1 @@
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo service postgresql stop
-sudo docker compose -f docker-compose.yaml up -d --build
+docker compose -f docker-compose.yaml up -d --build

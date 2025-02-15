17e23a69a3a860fd98fc7e7a75cd0168f8bc5cd9
snoopy910 <snoopy5473cool@gmail.com>
2025-02-15 20:15:48 +0000

    add algorand chains

--- common/chain.go
+++ common/chain.go
@@ -4,6 +4,9 @@ import "fmt"
 
 type Chain struct {
 	ChainId     string
+	GenesisId   string
+	GenesisHash string
+	IndexerUrl  string
 	ChainType   string
 	ChainUrl    string
 	KeyCurve    string
@@ -87,6 +90,33 @@ var Chains = []Chain{
 		ChainUrl:    "http://127.0.0.1:18443",
 		KeyCurve:    "ecdsa",
 		TokenSymbol: "BTC",
+	},
+	{
+		GenesisId:   "mainnet-v1.0",
+		GenesisHash: "wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=",
+		ChainType:   "algorand",
+		ChainUrl:    "https://mainnet-api.4160.nodely.dev",
+		IndexerUrl:  "https://mainnet-idx.4160.nodely.dev",
+		KeyCurve:    "ed25519",
+		TokenSymbol: "ALGO",
+	},
+	{
+		GenesisId:   "testnet-v1.0",
+		GenesisHash: "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
+		ChainType:   "algorand",
+		ChainUrl:    "https://testnet-api.4160.nodely.dev",
+		IndexerUrl:  "https://testnet-idx.4160.nodely.dev",
+		KeyCurve:    "ed25519",
+		TokenSymbol: "ALGO",
+	},
+	{
+		GenesisId:   "betanet-v1.0",
+		GenesisHash: "mFgazF+2uRS1tMiL9dsj01hJGySEmPN28B/TjjvpVW0=",
+		ChainType:   "algorand",
+		ChainUrl:    "https://betanet-api.4160.nodely.dev",
+		IndexerUrl:  "https://betanet-idx.4160.nodely.dev", // port 443?
+		KeyCurve:    "ed25519",
+		TokenSymbol: "ALGO",
 	},
 }
 

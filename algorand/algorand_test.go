dde05af9573cd32f0d975e29cbd7b4e42d8efc79
snoopy910 <snoopy5473cool@gmail.com>
2025-02-18 19:13:11 +0000

    test withdraw native and asa

--- algorand/algorand_test.go
+++ algorand/algorand_test.go
@@ -2,12 +2,15 @@ package algorand
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/StripChain/strip-node/common"
 	"github.com/algorand/go-algorand-sdk/client/v2/algod"
 	"github.com/algorand/go-algorand-sdk/client/v2/indexer"
+	"github.com/algorand/go-algorand-sdk/types"
 	"github.com/test-go/testify/assert"
 	"github.com/test-go/testify/mock"
 )
@@ -141,6 +144,171 @@ func TestSendAlgorandTransaction(t *testing.T) {
 				t.Errorf("expected error: %v", err)
 			}
 
+		})
+	}
+
+}
+
+func TestWithdrawAlgorandNativeGetSignature(t *testing.T) {
+	// Testnet
+
+	genesisHashStr := "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
+
+	// Decode the Base64 string to get the raw bytes.
+	genesisHashBytes, err := base64.StdEncoding.DecodeString(genesisHashStr)
+	if err != nil {
+		log.Fatalf("failed to decode genesis hash: %v", err)
+	}
+
+	tests := []struct {
+		Name            string
+		Account         string
+		Amount          string
+		Recipient       string
+		TokenAddr       string
+		IsError         bool
+		Error           string
+		SuggestedParams types.SuggestedParams
+	}{
+		{
+			Name: "Valid transaction",
+			SuggestedParams: types.SuggestedParams{
+				Fee:             1000,
+				FirstRoundValid: 100,
+				LastRoundValid:  200,
+				GenesisID:       "testnet-v1.0",
+				// GenesisHash is normally a 32-byte array encoded in base64.
+				GenesisHash: genesisHashBytes,
+			},
+			Account:   "IBEM5YYEA5UHMRKV3HPRPYB2IJZ4HIII5SW4EDCV5OJFHBKIJBMQWNME6U",
+			Amount:    "100",
+			Recipient: "ZQK6ICG4FMGMV35J5JPI7HPBVV6P4SBWY2PK35QSNQQ3SEADVHXYX3G7OQ",
+			IsError:   false,
+		},
+		{
+			Name: "Error in suggested params",
+			SuggestedParams: types.SuggestedParams{
+				Fee:             1000,
+				FirstRoundValid: 100,
+				LastRoundValid:  200,
+				GenesisID:       "testnet-v1.0",
+				// GenesisHash is normally a 32-byte array encoded in base64.
+				GenesisHash: genesisHashBytes,
+			},
+			Account:   "IBEM5YYEA5UHMRKV3HPRPYB2IJZ4HIII5SW4EDCV5OJFHBKIJBMQWNME6U",
+			Amount:    "100",
+			Recipient: "ZQK6ICG4FMGMV35J5JPI7HPBVV6P4SBWY2PK35QSNQQ3SEADVHXYX3G7OQ",
+			IsError:   true,
+			Error:     "failed to get suggested params: error in suggested params",
+		},
+	}
+
+	// Run test cases
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			clients := &MockClients{mockAlgod: new(MockAlgodClient), mockIndexer: new(MockIndexerClient)}
+			mockSuggestedParamsRequester := new(MockSuggestedParamsRequester)
+			clients.mockAlgod.On("SuggestedParams", mock.Anything).Return(mockSuggestedParamsRequester)
+			if tt.IsError {
+				mockSuggestedParamsRequester.On("Do", mock.Anything).Return(types.SuggestedParams{}, fmt.Errorf("error in suggested params"))
+			} else {
+				mockSuggestedParamsRequester.On("Do", mock.Anything).Return(tt.SuggestedParams, nil)
+			}
+			serializedTxn, dataToSign, err := clients.WithdrawAlgorandNativeGetSignature(tt.Account, tt.Amount, tt.Recipient)
+			fmt.Println(serializedTxn)
+			fmt.Println(dataToSign)
+			if !tt.IsError && err != nil {
+				t.Errorf("WithdrawAlgorandTokenGetSignature() error = %v", err)
+			} else if tt.IsError && err.Error() != tt.Error {
+				t.Errorf("expected error = %v", err)
+			}
+		})
+	}
+
+}
+
+func TestWithdrawAlgorandASAGetSignature(t *testing.T) {
+	// Testnet
+
+	genesisHashStr := "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
+
+	// Decode the Base64 string to get the raw bytes.
+	genesisHashBytes, err := base64.StdEncoding.DecodeString(genesisHashStr)
+	if err != nil {
+		log.Fatalf("failed to decode genesis hash: %v", err)
+	}
+
+	tests := []struct {
+		Name            string
+		Account         string
+		Amount          string
+		Recipient       string
+		AssetId         string
+		TransferType    string
+		IsError         bool
+		Error           string
+		SuggestedParams types.SuggestedParams
+	}{
+		{
+			Name: "Valid transaction",
+			SuggestedParams: types.SuggestedParams{
+				Fee:             1000,
+				FirstRoundValid: 100,
+				LastRoundValid:  200,
+				GenesisID:       "testnet-v1.0",
+				// GenesisHash is normally a 32-byte array encoded in base64.
+				GenesisHash: genesisHashBytes,
+			},
+			Account:      "IBEM5YYEA5UHMRKV3HPRPYB2IJZ4HIII5SW4EDCV5OJFHBKIJBMQWNME6U",
+			Amount:       "100",
+			Recipient:    "ZQK6ICG4FMGMV35J5JPI7HPBVV6P4SBWY2PK35QSNQQ3SEADVHXYX3G7OQ",
+			AssetId:      "10458941",
+			TransferType: "axfer",
+			IsError:      false,
+		},
+		{
+			Name: "Error in suggested params",
+			SuggestedParams: types.SuggestedParams{
+				Fee:             1000,
+				FirstRoundValid: 100,
+				LastRoundValid:  200,
+				GenesisID:       "testnet-v1.0",
+				// GenesisHash is normally a 32-byte array encoded in base64.
+				GenesisHash: genesisHashBytes,
+			},
+			Account:      "IBEM5YYEA5UHMRKV3HPRPYB2IJZ4HIII5SW4EDCV5OJFHBKIJBMQWNME6U",
+			Amount:       "100",
+			Recipient:    "ZQK6ICG4FMGMV35J5JPI7HPBVV6P4SBWY2PK35QSNQQ3SEADVHXYX3G7OQ",
+			AssetId:      "10458941",
+			TransferType: "axfer",
+			IsError:      true,
+			Error:        "failed to get suggested params: error in suggested params",
+		},
+	}
+
+	// Run test cases
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			clients := &MockClients{mockAlgod: new(MockAlgodClient), mockIndexer: new(MockIndexerClient)}
+			mockSuggestedParamsRequester := new(MockSuggestedParamsRequester)
+			clients.mockAlgod.On("SuggestedParams", mock.Anything).Return(mockSuggestedParamsRequester)
+			if tt.IsError {
+				mockSuggestedParamsRequester.On("Do", mock.Anything).Return(types.SuggestedParams{}, fmt.Errorf("error in suggested params"))
+			} else {
+				mockSuggestedParamsRequester.On("Do", mock.Anything).Return(tt.SuggestedParams, nil)
+			}
+			serializedTxn, dataToSign, err := clients.WithdrawAlgorandASAGetSignature(tt.Account, tt.Amount, tt.Recipient, tt.AssetId)
+			fmt.Println(serializedTxn)
+			fmt.Println(dataToSign)
+			fmt.Println(err)
+			if !tt.IsError && err != nil {
+				t.Errorf("WithdrawAlgorandTokenGetSignature() error = %v", err)
+			} else if tt.IsError && err.Error() != tt.Error {
+				t.Errorf("expected error = %v", err)
+			}
+			if !tt.IsError {
+				assert.Equal(t, string(dataToSign.Type), tt.TransferType)
+			}
 		})
 	}
 

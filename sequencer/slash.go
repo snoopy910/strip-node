6bbf4482bb91f52e375d2fd59d4fe4aeed2b3baa
snoopy910 <snoopy5473cool@gmail.com>
2025-03-24 15:26:02 +0200

    fix/update go routine and db updates

--- sequencer/slash.go
+++ sequencer/slash.go
@@ -1,122 +1,62 @@
 package sequencer
 
 import (
+	"context"
+	"encoding/hex"
 	"fmt"
 	"net/http"
 	"time"
 
 	intentoperatorsregistry "github.com/StripChain/strip-node/intentOperatorsRegistry"
 	"github.com/StripChain/strip-node/util/logger"
-	"github.com/go-pg/pg/v10"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
-
-var db *pg.DB
-
-type Heartbeat struct {
-	PublicKey string    `pg:"publickey,pk"`
-	Timestamp time.Time `pg:"timestamp"`
-}
 
 const (
 	CHECK_INTERVAL    = 5 * time.Minute
-	HEARTBEAT_TIMEOUT = 5 * time.Minute
+	HEARTBEAT_TIMEOUT = 2 * time.Hour
 )
 
-func AddHeartbeat(publicKey string) error {
-	heartbeat := &Heartbeat{
-		PublicKey: publicKey,
-		Timestamp: time.Now(),
-	}
-	_, err := db.Model(heartbeat).Insert()
+func UpdateSignersList() error {
+	// NOTE: ganache is not supporting notification and not able to subscribe new events
+	instance := intentoperatorsregistry.GetIntentOperatorsRegistryContract(RPC_URL, IntentOperatorsRegistryContractAddress)
+	filterOpts := &bind.FilterOpts{Context: context.Background()}
+	itr, err := instance.FilterSignerUpdated(filterOpts, nil)
 	if err != nil {
+		logger.Sugar().Errorw("Failed to filter old signerUpdated events", "error", err)
 		return err
 	}
-	return nil
-}
-
-func UpdateHeartbeat(publicKey string) error {
-	heartbeat := &Heartbeat{
-		PublicKey: publicKey,
-		Timestamp: time.Now(),
-	}
-	_, err := db.Model(heartbeat).
-		Set("timestamp = ?timestamp").
-		Where("publickey = ?publickey").
-		Update()
+	heartbeats, err := GetHeartbeats()
 	if err != nil {
+		logger.Sugar().Errorw("Failed to get heartbeats for already registered signer", "error", err)
 		return err
 	}
-	return nil
-}
-
-func GetHeartbeat(publicKey string) (Heartbeat, error) {
-	heartbeat := &Heartbeat{
-		PublicKey: publicKey,
-	}
-	err := db.Model(heartbeat).
-		Where("publickey = ?publickey").
-		Select()
-	if err != nil {
-		return Heartbeat{}, err
-	}
-	return *heartbeat, nil
-}
-
-func DeleteHeartbeat(publicKey string) error {
-	heartbeat := &Heartbeat{
-		PublicKey: publicKey,
-	}
-	_, err := db.Model(heartbeat).Delete()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func OnSignerRegistered(publicKey string) error {
-	// TODO: retrieve event when new signer is registered
-	signers, err := SignersList()
-	if err != nil {
-		logger.Sugar().Errorw("Failed to get active signers", "error", err)
-	}
-	newSigners := []string{}
-	for _, signer := range signers {
-		if _, err := GetHeartbeat(signer.PublicKey); err == nil {
-			newSigners = append(newSigners, signer.PublicKey)
-		}
-	}
-	for _, publicKey := range newSigners {
-		if err := AddHeartbeat(publicKey); err != nil {
-			logger.Sugar().Errorw("Failed to add heartbeat", "error", err)
+	for itr.Next() {
+		event := itr.Event
+		if event.Added {
+			if len(heartbeats) == 0 {
+				logger.Sugar().Infow("Add heartbeat for newly registered signer", "publickey", "0x"+hex.EncodeToString(event.Publickey[:]), "url", event.Url)
+				if err := AddHeartbeat("0x" + hex.EncodeToString(event.Publickey[:])); err != nil {
+					logger.Sugar().Errorw("Failed to add heartbeat for already registered signer", "error", err)
+				}
+			} else {
+				exist := false
+				for _, h := range heartbeats {
+					if hex.EncodeToString(event.Publickey[:]) == h.PublicKey {
+						exist = true
+						break
+					}
+				}
+				if !exist {
+					logger.Sugar().Infow("Add heartbeat for newly registered signer", "publickey", "0x"+hex.EncodeToString(event.Publickey[:]), "url", event.Url)
+					if err := AddHeartbeat("0x" + hex.EncodeToString(event.Publickey[:])); err != nil {
+						logger.Sugar().Errorw("Failed to add heartbeat for already registered signer", "error", err)
+					}
+				}
+			}
 		}
 	}
 	return nil
-}
-
-func IsSignerAlive(publicKey string) bool {
-	heartbeat := &Heartbeat{
-		PublicKey: publicKey,
-	}
-	err := db.Model(heartbeat).Last()
-	if err != nil {
-		return false
-	}
-	if time.Since(heartbeat.Timestamp) > HEARTBEAT_TIMEOUT {
-		return false
-	}
-	return true
-}
-
-func GetActiveSigners() ([]Signer, error) {
-	var signers []Signer
-	err := db.Model((*Heartbeat)(nil)).
-		ColumnExpr("distinct publickey").
-		Where("timestamp > ?", time.Now().Add(-HEARTBEAT_TIMEOUT)).
-		Select(&signers)
-	if err != nil {
-		return nil, err
-	}
-	return signers, nil
 }
 
 func CheckSignersStatus() {
@@ -127,49 +67,60 @@ func CheckSignersStatus() {
 	}
 
 	for _, signer := range signers {
-		go func(signer Signer) {
-			url := fmt.Sprintf("http://%s/health", signer.URL)
-			resp, err := http.Get(url)
+		logger.Sugar().Infow("Checking signer", "url", signer.URL)
+		url := fmt.Sprintf("%s/health", signer.URL)
+		resp, err := http.Get(url)
+		if err != nil {
+			logger.Sugar().Errorf("Signer %s is not working well, error: %v", signer, err)
+			return
+		}
+		if resp != nil && resp.StatusCode == http.StatusOK {
+			err := UpdateHeartbeat(signer.PublicKey)
 			if err != nil {
 				logger.Sugar().Errorf("Failed to update heartbeat for signer %s: %v", signer, err)
 				return
+			} else {
+				logger.Sugar().Infow("Updated heartbeat successfully", "heartbeat", signer.PublicKey, "timestamp", time.Now())
 			}
-			if resp != nil && resp.StatusCode == http.StatusOK {
-				UpdateHeartbeat(signer.PublicKey)
-			}
-			defer resp.Body.Close()
-		}(signer)
+		}
+		defer resp.Body.Close()
 	}
 }
 
 func startCheckingSigner() {
 	go func() {
-		time.Sleep(CHECK_INTERVAL)
+		for {
+			UpdateSignersList()
+			CheckSignersStatus()
 
-		CheckSignersStatus()
-
-		signers, err := GetActiveSigners()
-		if err != nil {
-			logger.Sugar().Errorw("Failed to get active signers", "error", err)
-			return
-		}
-		activeSigners := make(map[string]bool)
-		for _, signer := range signers {
-			activeSigners[signer.PublicKey] = true
-		}
-
-		instance := intentoperatorsregistry.GetIntentOperatorsRegistryContract(RPC_URL, IntentOperatorsRegistryContractAddress)
-		signers, err = SignersList()
-		if err != nil {
-			logger.Sugar().Errorw("Failed to get all signers", "error", err)
-			return
-		}
-
-		for _, signer := range signers {
-			if !activeSigners[signer.PublicKey] {
-				//TODO: Call the contract to apply detailed slashing logic
-				logger.Sugar().Infow("Slash signer", "signer", signer)
+			activeSigners, err := GetActiveSigners()
+			if err != nil {
+				logger.Sugar().Errorw("Failed to get active signers", "error", err)
+				return
 			}
+
+			activeSignersMap := make(map[string]bool)
+			for _, signer := range activeSigners {
+				activeSignersMap[signer.PublicKey] = true
+			}
+
+			// instance := intentoperatorsregistry.GetIntentOperatorsRegistryContract(RPC_URL, IntentOperatorsRegistryContractAddress)
+			signers, err := SignersList()
+			if err != nil {
+				logger.Sugar().Errorw("Failed to get all signers", "error", err)
+				return
+			}
+
+			for _, signer := range signers {
+				if !activeSignersMap[signer.PublicKey] {
+					//TODO: Call the contract to apply detailed slashing logic
+					logger.Sugar().Infow("Slash signer", "signer", signer)
+				} else {
+					logger.Sugar().Infow("Signer is acting properly", "signer", signer.URL)
+				}
+			}
+
+			time.Sleep(CHECK_INTERVAL)
 		}
 	}()
 }
